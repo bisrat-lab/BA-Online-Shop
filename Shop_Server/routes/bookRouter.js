7403f5ce6b0f84424249f7dc0f64ec66ebb54f02
asgdomhail@gmail.com <63181283+Asget-dev@users.noreply.github.com>
2021-07-12 07:55:09 -0700

    client side

--- Shop_Server/routes/bookRouter.js
+++ Shop_Server/routes/bookRouter.js
@@ -11,11 +11,14 @@ router.get('/:id',bookControllers.getBookById);
 
 //!Add new Books
 router.post('/',userController.authorizeAdmin,bookControllers.save);
+// router.post('/',bookControllers.save);
 
 //!Update existing Books
+// router.put('/:id',bookControllers.update);
 router.put('/:id',userController.authorizeAdmin,bookControllers.update);
 
 //!Delete Books by id 
+// router.delete('/:id',bookControllers.deleteById);
 router.delete('/:id',userController.authorizeAdmin,bookControllers.deleteById);
 
 
